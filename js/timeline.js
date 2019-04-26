80bd8c6cb3add2f3a5531b44d6b18131f73e1189
makio135 <makio135@gmail.com>
2019-04-26 12:06:36 +0200

    update slack link

--- js/timeline.js
+++ js/timeline.js
@@ -259,7 +259,7 @@ fetch(`https://randomuser.me/api/?results=${modules[currentModuleIndex].nb}&nat=
                         </div>
                         <div>
                             <p><strong>${user.name.first} ${user.name.last}</strong></p>
-                            <p><a href="https://bubble-af31447.slack.com/messages/DJ6KPCV24/" target="_blank"><img src="https://a.slack-edge.com/4f28/img/services/api_200.png" style="height:16px; width:16px; margin:-2px 0 ">&nbsp;<strong>@${user.name.first}.${user.name.last}</strong></a></p>
+                            <p><a href="https://bubble-af31447.slack.com/messages/DJ09UN28Z/" target="_blank"><img src="https://a.slack-edge.com/4f28/img/services/api_200.png" style="height:16px; width:16px; margin:-2px 0 ">&nbsp;<strong>@${user.name.first}.${user.name.last}</strong></a></p>
                             <!--<p><a href="mailto:${user.name.first}.${user.name.last}@edu.emlyon.com">${user.name.first}.${user.name.last}@edu.emlyon.com</a></p>-->
                             <p><svg viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>&nbsp;campus&nbsp;${campuses[Math.random() * campuses.length | 0]}</p>
                         </div>
