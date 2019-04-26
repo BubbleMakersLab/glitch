fe4b05188dae63101450459a3328b9c7ffa81520
makio135 <makio135@gmail.com>
2019-04-26 11:46:27 +0200

    push it all

--- js/timeline.js
+++ js/timeline.js
@@ -1,4 +1,7 @@
-//#FF5656
+function resizeEmpty(){
+    document.querySelector('#empty').style.height = `${document.querySelector('#bs').clientHeight}px`
+}
+
 // returns a gaussian random function with the given mean and stdev.
 function gaussian(mean, stdev) {
     let y2;
@@ -59,10 +62,12 @@ document.querySelector('h1').innerText = modules[currentModuleIndex].name
 
 const timeline = document.createElement('div')
 timeline.id = 'timeline'
-timeline.style.marginBottom = '60px'
+timeline.style.marginBottom = '10px'
+timeline.style.width = '90%'
+timeline.style.marginLeft = '5%'
 
-const parentEl = d3.select('.container').node()
-parentEl.insertBefore(timeline, parentEl.childNodes[0])
+const parentEl = document.querySelector('#bs')
+parentEl.appendChild(timeline)
 
 const svg = d3.select(timeline)
     .append('svg')
@@ -82,7 +87,7 @@ teacher.append('rect')
     .attr('y', '-5')
     .attr('width', '15')
     .attr('height', '10')
-    .attr('stroke', '#4A4A49')
+    .attr('stroke', () => currentModuleIndex < mean ? '#E1021A' : '#4A4A49')
     .attr('fill', 'white')
 
 teacher.append('circle')
@@ -101,16 +106,16 @@ teacher.append('circle')
     .attr('cx', 0)
     .attr('cy', 3)
     .attr('r', 2.5)
-    .attr('fill', '#4A4A49')
+    .attr('fill', () => currentModuleIndex < mean ? '#E1021A' : '#4A4A49')
 
 teacher.append('circle')
     .attr('cx', 0)
     .attr('cy', 11)
     .attr('r', 4)
-    .attr('fill', '#4A4A49')
+    .attr('fill', () => currentModuleIndex < mean ? '#E1021A' : '#4A4A49')
 
 teacher.append('path')
-    .attr('fill', '#4A4A49')
+    .attr('fill', () => currentModuleIndex < mean ? '#E1021A' : '#4A4A49')
     .attr('stroke', 'white')    
     .attr('d', 'M 0 -3 L 5 -1 L 0 1 L -5 -1 L 0 -3')
 
@@ -121,11 +126,16 @@ teacher.append('rect')
     .attr('height', 5)
     .attr('fill', 'white')
 
-teacher.append('text')
-    .text(() => currentModuleIndex < mean ? "you seem overdue" : "")
-    .attr('font-size', 10)
-    .attr('x', 18)
-    .attr('y', 3)
+if(currentModuleIndex < mean){
+    teacher.append('text')
+        .text("you seem overdue")
+        .attr('text-anchor', 'end')
+        .attr('font-size', 10)
+        .attr('x', -9)
+        .attr('y', 3)
+        .attr('fill', '#E1021A')
+
+}
 
 teacher.transition()
     .duration(3000)
@@ -220,14 +230,6 @@ groups.each(function(d, i) {
 })
 
 // participants view
-const p = document.createElement('p')
-p.style.marginTop = '2px'
-p.style.marginBottom = 0
-p.style.fontSize = '12px'
-p.innerHTML = `<span style="color:#2F61A8">${modules[currentModuleIndex].nb} participants</span> are reading this module:`
-
-timeline.appendChild(p)
-
 fetch(`https://randomuser.me/api/?results=${modules[currentModuleIndex].nb}&nat=fr`)
     .then(resp => resp.json())
     .then(json => {
@@ -242,17 +244,18 @@ fetch(`https://randomuser.me/api/?results=${modules[currentModuleIndex].nb}&nat=
         ]
         const div = document.createElement('div')
         div.classList.add('users')
-        div.innerHTML = json.results.reduce((html, user) => {
+        
+        div.innerHTML = `<p style="margin-top:2px; margin-bottom:0; font-size: 12px;"><span style="color:#2F61A8">${modules[currentModuleIndex].nb} participants</span> are reading this module:</p><br>` + json.results.reduce((html, user) => {
             return html + `<div class="user-wrap">
                 <img src="${user.picture.thumbnail}" alt="${user.name.first} ${user.name.last} profile picture">
                 <div class="user-card" style="width: 250px">
                     <div class="row">
                         <div>
-                            <img src="${user.picture.thumbnail}" alt="${user.name.first} ${user.name.last} profile picture">
+                            <img class="user-picture" src="${user.picture.thumbnail}" alt="${user.name.first} ${user.name.last} profile picture">
                         </div>
                         <div>
                             <p><strong>${user.name.first} ${user.name.last}</strong></p>
-                            <p><a href="https://bubble-af31447.slack.com" target="_blank"><img src="https://a.slack-edge.com/4f28/img/services/api_200.png" style="height:16px; width:16px; margin:-2px 0 ">&nbsp;<strong>@${user.name.first}.${user.name.last}</strong></a></p>
+                            <p><a href="https://bubble-af31447.slack.com/messages/DJ6KPCV24/" target="_blank"><img src="https://a.slack-edge.com/4f28/img/services/api_200.png" style="height:16px; width:16px; margin:-2px 0 ">&nbsp;<strong>@${user.name.first}.${user.name.last}</strong></a></p>
                             <!--<p><a href="mailto:${user.name.first}.${user.name.last}@edu.emlyon.com">${user.name.first}.${user.name.last}@edu.emlyon.com</a></p>-->
                             <p><svg viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>&nbsp;campus&nbsp;${campuses[Math.random() * campuses.length | 0]}</p>
                         </div>
@@ -261,5 +264,14 @@ fetch(`https://randomuser.me/api/?results=${modules[currentModuleIndex].nb}&nat=
             </div>`
         }, '')
 
-        timeline.appendChild(div)
+        document.querySelector('#participants').appendChild(div)
+
+        function resizeEmpty(){
+            console.log('test')
+            document.querySelector('#empty').style.height = `${document.querySelector('#bs').clientHeight}px`
+        }
+        document.querySelectorAll('.user-picture').forEach(img =>{
+            console.log(img);
+            img.addEventListener('load', resizeEmpty)
+        })
     })
